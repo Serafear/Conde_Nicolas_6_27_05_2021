6ecc2a522098f96e99c7133673236f675b313760
Nicolas <72019501+Serafear@users.noreply.github.com>
2021-06-11 12:23:59 +0200

    the like dont work yet

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,5 +1,6 @@
 /* 41-on importe thing depuis models */
 const Thing = require("../models/Thing");
+const User = require("../models/User");
 
 /* 42-pour avoir accès au système de fichier pour delete les données 
 dans la base de donnée */
@@ -10,7 +11,7 @@ exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce); /*
   pourquoi req.body.sauce marche et non req.body.thing ?
   
-  (ne pas oublier créer une limitation du poids d'envoi)*/
+  */
   delete sauceObject._id;
   const thing = new Thing({
     ...sauceObject,
@@ -42,16 +43,15 @@ exports.modifySauce = (req, res, next) => {
   Thing.findOne({ _id: req.params.id }) //nous utilisons l'ID que nous recevons comme paramètre pour accéder au Thing correspondant dans la base de données ;
     .then((thing) => {
       const filename = thing.imageUrl.split("/images/")[1]; //nous utilisons le fait de savoir que notre URL d'image contient un segment /images/ pour séparer le nom de fichier ;
-      fs.unlink(`images/${filename}`, () => {
-        //nous utilisons ensuite la fonction unlink du package fs pour supprimer ce fichier
-      });
-    })
-    .then(() => {
       const sauceObject = req.file //on regarde si un req.file existe, c-a-d si une image est présente dans la modification
         ? {
+            ...fs.unlink(`images/${filename}`, () => {
+              // les ... sont l'opérateur spread qui fait une copie des éléments associés.
+              //nous utilisons ensuite la fonction unlink du package fs pour supprimer ce fichier
+            }),
             //si oui il existe on remplace l'image
-            //par contre comment faire pour supprimer l'ancienne image pour qu'elle ne soit pas stockée dans images ?
-            ...JSON.parse(req.body.sauce),
+
+            ...JSON.parse(req.body.sauce), //le spread va copier le body.sauce de la request
             imageUrl: `${req.protocol}://${req.get("host")}/images/${
               //on utilise multer
               req.file.filename
@@ -59,7 +59,7 @@ exports.modifySauce = (req, res, next) => {
           }
         : {
             //si non il n'existe pas, on change juste les textes
-            ...req.body,
+            ...req.body, //le spread va copier le body de la request
           };
       Thing.updateOne(
         { _id: req.params.id },
@@ -68,15 +68,6 @@ exports.modifySauce = (req, res, next) => {
         .then(() => res.status(200).json({ message: "Objet modifié !" }))
         .catch((error) => res.status(400).json({ error }));
     });
-  /* Dans cette partie du code on dit à la machine de recupérer l'Id correspondate à l'objet
-    puis pour la commande modifier, d'unlink la photo précédente. Le unlink permet de supprimer
-    un fichier dans node.js du local file system. Pour le supprimer de la base de donnée mongo
-    on utilise deleteOne. Ici on veut simplement remplacer l'image. 
-    Donc on recupère l'élément en fonction de son id, on le vire du système local, puis on
-    pose la question: est ce qu'il y a un req.file, c-a-d, s'il y a une image. Si oui, on introduit la nouvelle photo
-    avec multer puis on fait la mise en jour dans la base de donnée avec updateOne.
-    Si non, on update simplement sans photo aka sns multer. 
-    */
 };
 
 exports.deleteSauce = (req, res, next) => {
@@ -106,18 +97,61 @@ exports.getAllSauce = (req, res, next) => {
 };
 
 /* le problème de la sauce c'est que le like doit persister même après qu'on back */
+/*
+Ce code crée une nouvelle sauce, ce n'est pas ce que je veux. l'objet pollue juste la 
+base de donnée, car la nouvelle sauce n'a aucune information de toute façon
+
 exports.sauceLike = (req, res, next) => {
-  Thing.findOne({
-    _id: req.params.id,
-  })
-    .then((thing) => {
-      res.status(200).json(thing);
+    const likeObject = JSON.parse(req.body.like);
+    delete likeObject._id;
+    const thing = new Thing({
+      ...likeObject,
     })
-    .catch((error) => {
-      res.status(404).json({
-        error: error,
+    thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet liké !" }))
+    .catch((error) => res.status(400).json({ error }));
+};*/
+
+//essai sauce, n°2
+exports.sauceVote = (req, res, next) => {
+  let like = req.params.like;
+  switch (like) {
+    case like === 1:
+      Thing.findOne({ _id: req.params.id }).then(() => {
+        Thing.updateOne(
+          { _id: req.params.id },
+          { $inc: { likes: 1 } },
+          { $push: { userId: 1 } }
+        )
+          .then(() => res.status(200).json({ message: "Objet liké !" }))
+          .catch((error) => res.status(400).json({ error }));
       });
-    });
+      break;
+
+    case like === 0:
+      Thing.findOne({ _id: req.params.id }).then(() => {
+        Thing.updateOne(
+          { _id: req.params.id },
+          { $inc: { likes: -1 } },
+          { $push: { userId: -1 } }
+        )
+          .then(() => res.status(200).json({ message: "Objet modifié !" }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+      break;
+
+    case like === -1:
+      Thing.findOne({ _id: req.params.id }).then(() => {
+        Thing.updateOne(
+          { _id: req.params.id },
+          { $inc: { dislikes: 1 } },
+          { $push: { userId: 1 } }
+        )
+          .then(() => res.status(200).json({ message: "Objet disliké !" }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+  }
 };
 
 //on passe ensuite à app.js pour terminer toutes les déclarations
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -25,7 +25,7 @@ router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, multer, sauceCtrl.sauceLike);
+router.post('/:id/like', auth, multer, sauceCtrl.sauceVote);
 //avec auth les routes sont sécurisées et avec multer l'utilisateur peut modifier les données
 
 /* 35-On prépare le module à l'exportation */
